4ac46f28ad7ea4537ce304964544f7492b80872d
ew00github <143366282+ew00github@users.noreply.github.com>
2023-11-03 21:18:04 -0400

    Renamed methods in the HttpCatService class to make their purpose more clear. Moved the JSONConverter to the LoadDatabase class so that the HttpCatService methods only use HttpCatRepository's methods.
    Additionally, I found a bug preventing the program from running should the H2 Database be reset or empty to begin with, and I added a line to application.properties that should fix this.

--- src/main/java/com/evan/HTTPCatClone/repository/LoadDatabase.java
+++ src/main/java/com/evan/HTTPCatClone/repository/LoadDatabase.java
@@ -1,11 +1,13 @@
 package com.evan.HTTPCatClone.repository;
 
 import com.evan.HTTPCatClone.service.HttpStatusService;
+import com.evan.HTTPCatClone.tools.JSONConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
@@ -16,7 +18,8 @@ class LoadDatabase {
                 log.info("All statuses found!");
                 return;
             }
-            repository.saveAll(service.get());
+            JSONConverter converter = new JSONConverter();
+            repository.saveAll(converter.convert());
             log.info("Database pre-populated successfully");
         };
     }
