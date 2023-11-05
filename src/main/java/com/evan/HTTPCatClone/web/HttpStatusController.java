d0a08c034d97a9b860b0583c19a8051732674d39
ew00github <143366282+ew00github@users.noreply.github.com>
2023-11-05 11:37:54 -0500

    Created a new endpoint "/statusGroup/{status}" that allows user to view all status codes within a range of 99 codes. Ex. All codes 100-199, 200-299, etc., where the {status} value must be 100, 200, 300, 400, or 500, otherwise a bad request error will be returned.

--- src/main/java/com/evan/HTTPCatClone/web/HttpStatusController.java
+++ src/main/java/com/evan/HTTPCatClone/web/HttpStatusController.java
@@ -2,12 +2,12 @@ package com.evan.HTTPCatClone.web;
 
 import com.evan.HTTPCatClone.model.HttpStatus;
 import com.evan.HTTPCatClone.service.HttpStatusService;
-import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import java.util.ArrayList;
 
 @RestController
 public class HttpStatusController {
@@ -15,7 +15,7 @@ public class HttpStatusController {
     private final HttpStatusService httpStatusService;
 
     public HttpStatusController(HttpStatusService httpStatusService) {
-        this.HttpStatusService = httpStatusService;
+        this.httpStatusService = httpStatusService;
     }
 
     @GetMapping("/")
@@ -30,7 +30,7 @@ public class HttpStatusController {
 
     @GetMapping("/id/{id}")
     public ResponseEntity<byte[]> download(@PathVariable Long id){
-        HttpStatus httpStatus = HttpStatusService.getById(id);
+        HttpStatus httpStatus = httpStatusService.getById(id);
         if (httpStatus == null) throw new ResponseStatusException(org.springframework.http.HttpStatus.NOT_FOUND);
         byte[] image = httpStatus.getImage();
         HttpHeaders headers = new HttpHeaders();
@@ -40,9 +40,9 @@ public class HttpStatusController {
 
     @GetMapping("/status/{status}")
     public ResponseEntity<byte[]> download(@PathVariable String status){
-        HttpStatus httpStatus = HttpStatusService.getByStatus(status);
+        HttpStatus httpStatus = httpStatusService.getByStatus(status);
         if (httpStatus == null) {
-        httpStatus = HttpStatusService.getByStatus("404");
+        httpStatus = httpStatusService.getByStatus("404");
         }
         byte[] image = httpStatus.getImage();
         HttpHeaders headers = new HttpHeaders();
@@ -50,9 +50,25 @@ public class HttpStatusController {
         return new ResponseEntity<>(image, headers, org.springframework.http.HttpStatus.OK);
     }
 
+    @GetMapping("/statusGroup/{status}")
+    public ArrayList<String> getStatusGroup(@PathVariable String status){
+        ArrayList<String> statusGroup = new ArrayList<>();
+        if (!(status.equals("100") || status.equals("200") || status.equals("300") || status.equals("400") || status.equals("500"))){
+            throw new ResponseStatusException(org.springframework.http.HttpStatus.BAD_REQUEST);
+        }
+        int upperBound = Integer.parseInt(status) + 99;
+        for (int i = Integer.parseInt(status); i < upperBound; i++){
+            HttpStatus httpStatus = httpStatusService.getByStatus(String.valueOf(i));
+            if ((httpStatus != null && httpStatus.getStatus() != null)){
+                statusGroup.add(httpStatusService.getByStatus(String.valueOf(i)).getStatus());
+            }
+        }
+        return statusGroup;
+    }
+
     @PostMapping("/save")
     public HttpStatus save(@RequestBody HttpStatus httpStatus) {
-        return HttpStatusService.save(httpStatus);
+        return httpStatusService.save(httpStatus);
     }
 }
 
