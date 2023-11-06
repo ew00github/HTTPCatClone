52efa2c06d4a6a0ed3cfd388cf9ad305956ab4c2
ew00github <143366282+ew00github@users.noreply.github.com>
2023-11-05 22:07:10 -0500

    Added a new Service class for the WebController. Added a WebClientConfig class for configuring and injecting WebClient Beans.

--- src/main/java/com/evan/HTTPCatClone/web/WebController.java
+++ src/main/java/com/evan/HTTPCatClone/web/WebController.java
@@ -1,33 +1,23 @@
 package com.evan.HTTPCatClone.web;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import com.evan.HTTPCatClone.service.WebService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class WebController {
 
-    private final WebClient webClient;
+    private final WebService webService;
 
-    public WebController() {
-        this.webClient = WebClient.create();
+    public WebController(WebService webService){
+        this.webService = webService;
     }
+
     @GetMapping("/catAPI/status/{status_code}")
-    public Mono<ResponseEntity<byte[]>> getStatusFromCat(@PathVariable String status_code) {
-        Mono<byte[]> imageBytes =  webClient.get()
-                .uri("https://http.cat/" + status_code)
-                .retrieve()
-                .bodyToMono(byte[].class);
-        return imageBytes.map(catImage -> {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.IMAGE_JPEG);
-            return new ResponseEntity<>(catImage, headers, org.springframework.http.HttpStatus.OK);
-        });
+    public Mono<ResponseEntity<byte[]>> getStatusFromCat(@PathVariable String status_code){
+        return this.webService.getStatusFromCat(status_code);
     }
 }
