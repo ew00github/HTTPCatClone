2daf4c97e9d3ef674dcfcf4d2de90bff458f0beb
ew00github <143366282+ew00github@users.noreply.github.com>
2023-11-15 20:02:51 -0500

    Created Endpoints for a third party application to receive images from the HttpCatClone.

--- src/main/java/com/evan/HTTPCatClone/service/WebService.java
+++ src/main/java/com/evan/HTTPCatClone/service/WebService.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -14,9 +15,12 @@ public class WebService {
 
     private final WebClient webClient;
 
+    private final HttpStatusService statusService;
+
     @Autowired
-    public WebService(WebClient webClient) {
+    public WebService(WebClient webClient, HttpStatusService statusService) {
         this.webClient = webClient;
+        this.statusService = statusService;
     }
 
     public Mono<ResponseEntity<byte[]>> getStatusFromCat(String statusCode) {
@@ -29,5 +33,26 @@ public class WebService {
             headers.setContentType(MediaType.IMAGE_JPEG);
             return new ResponseEntity<>(catImage, headers, org.springframework.http.HttpStatus.OK);
         });
+    }
+
+    public ResponseEntity<byte[]> sendImageFromId(Long id){
+        byte[] imageBytes = statusService.getById(id).getImage();
+        if(imageBytes == null) throw new ResponseStatusException(org.springframework.http.HttpStatus.NOT_FOUND);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        return new ResponseEntity<>(imageBytes, headers, org.springframework.http.HttpStatus.OK);
+    }
+
+    public ResponseEntity<byte[]> sendImageFromStatus(String status){
+        byte[] imageBytes = statusService.getByStatus(status).getImage();
+        if(imageBytes == null) throw new ResponseStatusException(org.springframework.http.HttpStatus.NOT_FOUND);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        return new ResponseEntity<>(imageBytes, headers, org.springframework.http.HttpStatus.OK);
+    }
+
+    public Mono<String> sendStatusFromId(Long id){
+        String statusCode = statusService.getById(id).getStatus();
+        return Mono.just(statusCode);
     }
 }
--- src/main/java/com/evan/HTTPCatClone/web/WebController.java
+++ src/main/java/com/evan/HTTPCatClone/web/WebController.java
@@ -1,5 +1,6 @@
 package com.evan.HTTPCatClone.web;
 
+
 import com.evan.HTTPCatClone.service.WebService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +13,27 @@ public class WebController {
 
     private final WebService webService;
 
-    public WebController(WebService webService){
+    public WebController(WebService webService) {
         this.webService = webService;
     }
 
     @GetMapping("/catAPI/status/{statusCode}")
     public Mono<ResponseEntity<byte[]>> getStatusFromCat(@PathVariable String statusCode){
         return this.webService.getStatusFromCat(statusCode);
+    }
+
+    @GetMapping("/catAPI/sendImage/id/{id}")
+    public ResponseEntity<byte[]> sendStatusFromId(@PathVariable Long id){
+        return this.webService.sendImageFromId(id);
+    }
+
+    @GetMapping("/catAPI/sendImage/status/{status}")
+    public ResponseEntity<byte[]> sendStatusFromStatus(@PathVariable String status){
+        return this.webService.sendImageFromStatus(status);
+    }
+
+    @GetMapping("/catAPI/sendStatusCode/id/{id}")
+    public Mono<String> sendStatusCodeFromId(@PathVariable Long id){
+        return this.webService.sendStatusFromId(id);
     }
 }
